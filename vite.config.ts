702c5ed2379c97e1026ace2d2b1a1254a4883aed
bagu1997 <107236639+bagu1997@users.noreply.github.com>
2025-12-25 22:21:50 +0530

    Update vite.config.ts by adding /

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bagavathyraja',
+  base: '/bagavathyraja/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
